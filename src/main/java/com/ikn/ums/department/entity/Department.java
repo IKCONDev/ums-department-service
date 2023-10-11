167935c9a4f2456dc7da3cbc50802fda1ce760d0
MaheshJarati <MaheshJarati@IKCON1019>
2023-10-11 17:17:00 +0530

    database changes performed

--- src/main/java/com/ikn/ums/department/entity/Department.java
+++ src/main/java/com/ikn/ums/department/entity/Department.java
@@ -2,11 +2,14 @@ package com.ikn.ums.department.entity;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.Columns;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,7 +19,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "department")
+@Table(name = "department_tab")
 public class Department {
 
     @Id
